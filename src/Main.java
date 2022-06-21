f6f71dfeee0422f1c0926cc993e443acab08bdaa
dmitrymikita <wmists@gmail.com>
2022-06-22 00:40:07 +1000

    Variable correction

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int balance = 100; //текущий баланс
         int payment = 2350; //сумма платежа
-        int priceonebonus = 100; // шаг для начисления бонуса (рублей)
+        int priceOneBonus = 100; // шаг для начисления бонуса (рублей)
 
         if (payment > 1000) {
-            int bonus = (payment / priceonebonus);
+            int bonus = (payment / priceOneBonus);
             System.out.println("Начислено ,бонусов:" + bonus);
             System.out.println("Ваш баланс:" + (balance + payment + bonus));
         } else {
